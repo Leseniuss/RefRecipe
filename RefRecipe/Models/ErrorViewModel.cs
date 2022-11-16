15ea235a1e7d22d4197e7eb5d838725a97a240c0
Jyri Leskinen <jleskinen@hotmail.com>
2022-11-16 19:23:19 +0200

    Git testi

---
+++ RefRecipe/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RefRecipe.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
